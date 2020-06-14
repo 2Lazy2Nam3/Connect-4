cb28ac4b676a4430a0836e377dcec94cb7d5f998
Derek <derekmcdermid23@gmail.com>
2020-06-13 21:59:58 -0600

    X AND O

--- Connect-4/main.cpp
+++ Connect-4/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	std::cout << "______________________\n";
+	std::cout << "-----------------------------\n";
 	for (int i = 0; i < 6; ++i) {
 		std::cout << "|";
-		for (int n = 0; n < 7; ++n) {
-			std::cout << "__|";
+		for (int j = 0; j < 7; ++j) {
+			if (j % 2 == 0)
+				std::cout << " X ";
+			else
+				std::cout << " O ";
+			std::cout << "|";
 		}
-		std::cout << "\n";
+		std::cout << "\n-----------------------------\n";
 	}
 }
